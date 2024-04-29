c6a6302b823a0890ac173a184e4faf501d8857fa
Rostbr4ten <stumpfalexander@gmx.de>
2024-04-29 12:35:19 +0200

    REMOVE unnecessary imports

--- category-service/src/main/java/com/hka/webshop/CategoryController.java
+++ category-service/src/main/java/com/hka/webshop/CategoryController.java
@@ -2,22 +2,15 @@ package com.hka.webshop;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.servlet.http.HttpServletResponse;
-
-import java.util.Objects;
-
 
 @RestController()
 @RequestMapping(path="/categories")
 public class CategoryController {
 
-    private final String productServiceEndpoint = !Objects.equals(System.getenv("PRODUCT_ENDPOINT"), "") ? System.getenv("PRODUCT_ENDPOINT") : "localhost";
     private final CategoryRepository categoryRepository;
 
     @Autowired
@@ -50,23 +43,9 @@ public class CategoryController {
     public ResponseEntity<?> deleteCategory(@PathVariable Long id, HttpServletResponse response){
         response.setHeader("Pod", System.getenv("HOSTNAME"));
         if(!categoryRepository.existsById(id)) throw new RuntimeException();
-        if(checkProductsExist(id)) return ResponseEntity.badRequest().body("Cannot delete category if products using this category exist");
 
         categoryRepository.deleteById(id);
         return ResponseEntity.ok("");
     }
 
-    private boolean checkProductsExist(Long categoryId){
-        WebClient client = WebClient.create("http://" + productServiceEndpoint + ":8080/products/");
-        try {
-            var response = client.get().uri("?categoryId="+String.valueOf(categoryId)).accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(Product[].class).block();
-            assert response != null;
-            return response.length >0;
-        } catch (WebClientResponseException wcre) {
-            return false;
-        }catch (Exception e){
-            System.out.println(e);
-            return true;
-        }
-    }
 }
